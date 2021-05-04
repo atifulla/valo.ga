f45e7468a5e04ba4f964eee5b1cd4873acefff31
Atif-coder <atifulla1@gmail.com>
2021-05-04 19:02:38 +0530

    made responsive commit

--- src/Clips.js
+++ src/Clips.js
@@ -10,24 +10,24 @@ function Clips() {
         <a className="goto" href="https://playvalorant.com/en-us/news/">GO TO NEWSPAGE</a>
       </div>
       <div className="images">
-        <div className="size">
-          <img className="size1"
+        <div className="">
+          <img className="size"
             src="https://i.ytimg.com/vi/Wrdh5HrOCMc/maxresdefault.jpg"
             alt="Logo"
           />
           <h3>Updating the Privacy Notice and Terms of Service</h3>
         </div>
 
-        <div className="size">
-          <img className="size1"
+        <div className="">
+          <img className="size"
             src="https://specials-images.forbesimg.com/imageserve/5ef1b00e8548c80006be762b/960x0.jpg?fit=scale"
             alt="Logo"
           />
           <h3>VCT CHALLENGERS NA AND AIM LAB ANNOUNCE THE AIM LAB COMBINE</h3>
         </div>
 
-        <div className="size">
-          <img className="size1"
+        <div className="">
+          <img className="size"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS86MwG7YF3zSaFfA16AyKttaB1Cly44u4FXQ&usqp=CAU"
             alt="Logo"
           />
